c0ca4cc8895e4d3f05d8279ae9fc3be9887e32a7
Slotmachine <worapong_005@hotmail.com>
2022-04-20 15:49:36 +0700

    edit ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'library';
+  restData: any=[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.getData()
+
+  }
+
+  
+  getData(){
+    fetch(`https://cloud-3001.lib.cmu.ac.th/exam/room/`).then(e=>{
+      return e.json()
+    }).then(e=>{
+      console.log(e)
+      this.restData = e
+    })
+  }
 }
